25c1b4e2f7499d1a9e07104f8a947fda71f83b52
Henrique Lima <henriquelima.dev@gmail.com>
2024-08-19 20:07:09 +0200

    Add Argument for data_path and recursive

--- db_build.py
+++ db_build.py
@@ -3,6 +3,7 @@
 # =========================
 import box
 import yaml
+import argparse
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
@@ -14,10 +15,12 @@ with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
 
 
 # Build vector database
-def run_db_build():
-    loader = DirectoryLoader(cfg.DATA_PATH,
+def run_db_build(data_path, recursive):
+    loader = DirectoryLoader(data_path,
                              glob='*.pdf',
-                             loader_cls=PyPDFLoader)
+                             loader_cls=PyPDFLoader,
+                             recursive=recursive,
+                             show_progress=True)
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=cfg.CHUNK_SIZE,
                                                    chunk_overlap=cfg.CHUNK_OVERLAP)
@@ -30,4 +33,13 @@ def run_db_build():
     vectorstore.save_local(cfg.DB_FAISS_PATH)
 
 if __name__ == "__main__":
-    run_db_build()
+    # Argument parser setup
+    parser = argparse.ArgumentParser(description="Build FAISS Vector Database")
+    parser.add_argument('-p', type=str, default=cfg.DATA_PATH,
+                        help="Path to the directory containing PDF files. Default is set in config.")
+    parser.add_argument('-r', type=bool, default=True,
+                        help="Whether to load files recursively. Default is True.")
+    
+    args = parser.parse_args()
+    
+    run_db_build(args.data_path, args.recursive)
